0ae232764beb36d506d175b2ab827be5bea8dfd7
Robert <robert.warmack@gmail.com>
2017-05-11 10:55:14 -0500

    Default credentials.

--- bin/registries/docker-pull.gcr.sh
+++ bin/registries/docker-pull.gcr.sh
@@ -16,6 +16,7 @@ fi
 # This could be moved in large part to the "pre" step, but not entirely.
 # Leaving it here gives us the flexibility to do cluster/project based on branch.
 KEYFILE=${HOME}/gcloud-service-key.json
+export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
 echo $GCLOUD_KEY | base64 --decode > "${KEYFILE}"
 gcloud auth activate-service-account --key-file "${KEYFILE}" --project "${GCP_PROJECT}"
 
@@ -23,8 +24,8 @@ gcloud config set project "${GCP_PROJECT}"
 gcloud config set container/cluster "${GCP_CLUSTER}"
 gcloud config set compute/zone "${GCP_ZONE}"
 
-gcloud container clusters get-credentials "${GCP_CLUSTER}" --key-file "${KEYFILE}" 
-gcloud auth application-default login
+gcloud container clusters get-credentials "${GCP_CLUSTER}"
+#gcloud auth application-default login
 
 # Authorize the docker client to work with GCR
 gcloud docker --authorize-only
