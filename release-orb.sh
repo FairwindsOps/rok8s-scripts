4c37adc8d2df763eabed8b49a362ddf862fb8109
Andrew Suderman <andrew@sudermanjr.com>
2019-07-24 12:29:27 -0600

    Better output on release script. Fixing slack message

--- release-orb.sh
+++ release-orb.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+echo "Starting release."
+
+
 command -v circleci >/dev/null 2>&1 || { echo >&2 "I require circleci but it's not installed.  Aborting."; exit 1; }
+echo "Found circleci command."
 
 commit=$(git log -n1 --pretty='%h')
 tag=$(git describe --exact-match --tags "$commit")
 
 retVal=$?
+echo "retVal = $retVal"
 if [ $retVal -ne 0 ]; then
     echo "You need to checkout a valid tag for this to work."
+    exit $retVal
 fi
-exit $retVal
 
-echo "Releasing orb for ${tag:1}"
+echo "Release: $commit - $tag"
 
+echo "Validating..."
 make orb-validate || { echo 'Orb failed to validate.' ; exit 1; }
 
+echo "Releasing..."
 circleci orb publish orb.yml "fairwinds/rok8s-scripts@${tag:1}"
