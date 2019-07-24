159a1ed61361638c3e56fd920cd807822f7f4c88
Andrew Suderman <andrew@sudermanjr.com>
2019-07-24 15:27:13 -0600

    Added version checking to release script. Fixes #288. Also fix bugs with examples in orb

--- release-orb.sh
+++ release-orb.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-echo "Starting release."
+function version_gt() {
+    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
+}
 
+echo "Starting release."
 
 command -v circleci >/dev/null 2>&1 || { echo >&2 "I require circleci but it's not installed.  Aborting."; exit 1; }
 echo "Found circleci command."
+
+cli_version=$(circleci version | cut -d+ -f1)
+required_version=0.1.5705
+
+if version_gt "$required_version" "$cli_version"; then
+     echo "This script requires circleci version greater than or equal to 0.1.5705!"
+     exit 1
+fi
 
 commit=$(git log -n1 --pretty='%h')
 tag=$(git describe --exact-match --tags "$commit")
