2f6878d50c0e533fe2798b70a255cb9cf3b64a4e
Robert Brennan <accounts@rbren.io>
2020-09-08 17:51:01 -0400

    Update setup.sh

--- test/setup.sh
+++ test/setup.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:$(pwd)/bin
 
 echo "Installing Shellcheck"
 scversion="v0.5.0"
-wget "https://storage.googleapis.com/shellcheck/shellcheck-$scversion.linux.x86_64.tar.xz"
+wget "https://github.com/koalaman/shellcheck/releases/download/$scversion/shellcheck-$scversion.linux.x86_64.tar.xz"
 tar --xz -xvf "shellcheck-$scversion.linux.x86_64.tar.xz"
 mv shellcheck-$scversion/shellcheck /usr/bin/shellcheck
 chmod +x /usr/bin/shellcheck
